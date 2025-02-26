ca03792c39d8c2dd31a575d7caf255685ee1fbee
Keshav Chandel <chandelkeshav4@gmail.com>
2025-02-26 23:14:44 +0530

    created routes

--- Backend/src/config/passport.js
+++ Backend/src/config/passport.js
@@ -1,24 +1,40 @@
 require('dotenv').config();
-const User = require('../models/User');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/User');
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:"http://localhost:500/auth/google/callback",
+    callbackURL: "http://localhost:5000/auth/google/callback", 
 },
 
-async(accessToken, refreshToken, profile, cb)=>{
-   let user = await User.findOne({googleId: profile.id});
-   if(!user){
-    user = new User({name:profile.displayName, email:profile.email[0].value, googleId:profile.id});
-   } 
-   done(null, user);
-}
-));
+async (accessToken, refreshToken, profile, cb) => {
+    try {
+        let user = await User.findOne({ googleId: profile.id });
 
-passport.serializeUser((user, done)=>{
-    done(null,user);
+        if (!user) {
+            user = new User({
+                name: profile.displayName,
+                email: profile.emails[0].value, 
+                googleId: profile.id
+            });
+
+            await user.save(); // Save new user
+        }
+
+        cb(null, user); 
+    } catch (err) {
+        cb(err, null);
+    }
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id); // Serialize only user ID
 });
-passport.deserializeUser((id, done) => User.findById(id, (err, user)=> done(null,user)));
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => done(err, user));
+});
+
+module.exports = passport;
--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,10 +1,13 @@
-const {signup, login, resetPassword, googleAuth} = require('../controllers/authController');
-// const googleAuth  = require('../config/passport');
+const { signup, login, resetPassword } = require('../controllers/authController');
+const { googleAuth } = require('../config/passport');
 const express = require('express');
-const router =  express.Router();
-//  Api end points
-router.post("/signup",signup);
-router.post("/login",login);
-router.post("/reset-password",resetPassword);
-router.post("/google-auth",googleAuth);
+
+const router = express.Router();
+
+// API Endpoints
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/reset-password", resetPassword);
+router.post("/google-auth", googleAuth);
+
 module.exports = router;
