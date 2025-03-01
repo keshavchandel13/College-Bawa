849f756120621c4bafb5b8eda23fd3db4f734761
monkji1250 <154007033+monkji1250@users.noreply.github.com>
2025-03-01 23:36:15 +0530

    fp

--- Frontend/src/features/auth/forgetpassword.jsx
+++ Frontend/src/features/auth/forgetpassword.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import { AiFillGoogleCircle } from 'react-icons/ai';
+import Home from '../../pages/Home';
+import "../../styles/auth/forgotpassword.css";  
+
 
 function forgetpassword() {
   return (
