c7f9998ba6f4e375548b070c91462d1716441408
monkji1250 <154007033+monkji1250@users.noreply.github.com>
2025-03-01 15:31:00 +0530

    Frontend files updated .Login page updation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Login from './features/auth/Login'
+import './styles/global.css'
 export default function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      {/* <h1>Hello</h1> */}
+      <Login/>
       
     </div>
   )
--- Frontend/src/features/auth/Login.jsx
+++ Frontend/src/features/auth/Login.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-
+<link rel="stylesheet" href="login.css" />
+import "../../styles/auth/login.css"
 function Login() {
   return (
-    <div>
-        
-
+    <div className='login-page'>
+        <div className="container">
+          <h2>Login</h2>
+          <input type="email" name="email" id="" placeholder='Email' />
+          <input type="password" name="password" id="" placeholder='Password'/>
+          <button type="button"> Login</button>
+          <button type="button">Forgot password?</button>
+        </div>
       
     </div>
+    
   )
 }
 
