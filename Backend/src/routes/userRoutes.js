cee3f5bdcc5262c9ab5b6916e82aba485252fd3e
Keshav Chandel <chandelkeshav4@gmail.com>
2025-04-18 00:15:06 +0530

    debug profile page

--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -4,11 +4,10 @@ const authMiddleware = require('../middlewares/authMiddleware');
 const { getAllUsers, getUserById, updateUserProfile } = require('../controllers/userController');
 const upload = require("../middlewares/multer")
 
-//Cloudinary file upload 
-// const upload = require("upload");
+
 const router = express.Router();
 
-router.post('/add-user-details', upload.single("profileImage", addUserDetails));
+router.post('/addUserDetails', upload.single("profileImage"), addUserDetails);
 
 // Protected Routes using authMiddleware
 router.get('/all-users', authMiddleware, getAllUsers);
--- Frontend/src/api/profile.js
+++ Frontend/src/api/profile.js
@@ -7,10 +7,10 @@ export const submitUserProfile = async (formData) => {
   try {
     const data = new FormData();
     Object.entries(formData).forEach(([key, value]) => {
+      console.log("Appending:", key, value);
         data.append(key, value);
     });
-
-    const res = await axios.post(`http://localhost:5000/api/user/add-user-details`, data, {
+    const res = await axios.post(`http://localhost:5000/api/user/addUserDetails`, data, {
       headers: { "Content-Type": "multipart/form-data" },
     });
 
