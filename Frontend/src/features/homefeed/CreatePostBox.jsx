cf9ef5b963154f427c41f177c6cf0499e8a9a678
monkji1250 <154007033+monkji1250@users.noreply.github.com>
2025-07-21 22:26:56 +0530

    POSTBOX PHASE 1

--- Frontend/src/features/homefeed/CreatePostBox.jsx
+++ Frontend/src/features/homefeed/CreatePostBox.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
-// import '../../styles/homepage/createPostBox.css';
+import '../../styles/homepage/createpostbox.css';
+
+import { FaPhotoVideo, FaPoll, FaSmile, FaMapMarkerAlt, FaPlus } from 'react-icons/fa';
 
 export default function CreatePostBox({ openModal }) {
   const profilePic = localStorage.getItem("profileImage");
+
   return (
-    <div className="create-post-box">
-      <img src={profilePic?profilePic:'/default.jpg'} alt="Your Avatar" />
-      <input
-        type="text"
-        onClick={openModal}
-        placeholder="What's on your mind?"
-        readOnly
-      />
-      <button onClick={openModal}>Post</button>
+    <div className="post-box">
+      <div className="top-section">
+        <img src={profilePic || '/default.jpg'} alt="Avatar" className="avatar" />
+       <textarea
+  className="post-input"
+  placeholder="What's happening on campus today?"
+  onClick={openModal}
+  readOnly
+  rows={6}
+/>
+
+      </div>
+      <div className="bottom-section">
+        <div className="post-options">
+          <button><FaPhotoVideo /> Photo</button>
+          <button><FaPoll /> Poll</button>
+          <button><FaSmile /> Feeling</button>
+          <button><FaMapMarkerAlt /> Location</button>
+           <button className="post-btn" onClick={openModal}>
+          <FaPlus /> Post </button>
+        </div>
+       
+       
+      </div>
     </div>
   );
 }
