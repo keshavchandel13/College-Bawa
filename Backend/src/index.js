4824a01baf14e5554ce9e955753bceee4668f2d2
Keshav Chandel <chandelkeshav4@gmail.com>
2025-09-05 15:05:41 +0530

    Backend fixed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -43,7 +43,7 @@ connectDB();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: `${process.env.FRONTEND_URL}`,
+    origin: process.env.FRONTEND_URL,
     credentials: true
   }
 });
