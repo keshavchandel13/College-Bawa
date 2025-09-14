cf2396d3e95f5fcfbeabd40a4551f10508c968b1
Keshav Chandel <chandelkeshav4@gmail.com>
2025-09-15 00:19:09 +0530

    chat theme fixed

--- Frontend/src/components/chat/Navbar.jsx
+++ Frontend/src/components/chat/Navbar.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import "../../styles/chat/Navbar.css";
 
-const Navbar = ({ selectedUser }) => {
+const ChatNavbar = ({ selectedUser }) => {
   return (
-    <div className="navbar">
-      <div className="navbar-content">
+    <div className="chat-navbar" id="chat-navbar">
+      <div className="chat-navbar-content">
         <img
           src={selectedUser?.profileImage || "/default.jpg"}
           alt="Profile"
-          className="chat-image"
+          className="chat-navbar-image"
         />
-        <span className="user-name">{selectedUser?.name || "User"}</span>
+        <span className="chat-navbar-username">
+          {selectedUser?.name || "User"}
+        </span>
       </div>
     </div>
   );
 };
 
-export default Navbar;
+export default ChatNavbar;
