ddcb396ff9b9bd41fd316356dd589a7c6b226a97
Keshav Chandel <chandelkeshav4@gmail.com>
2025-10-10 18:52:29 +0530

    cors fixed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 // CORS setup
 const corsOptions = { 
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
