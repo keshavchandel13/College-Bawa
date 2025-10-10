c1f387f8993e7881db353bd9ba441669d5207d18
Keshav Chandel <chandelkeshav4@gmail.com>
2025-10-10 19:22:08 +0530

    profile fixed

--- Frontend/src/api/profile.js
+++ Frontend/src/api/profile.js
@@ -38,7 +38,7 @@ export const getUserProfile = async (token) => {
 
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", 
+  baseURL: import.meta.env.VITE_APP_BACKEND_URL, 
 });
 
 // Add token from localStorage 
